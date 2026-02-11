feaf31a8bb0ec121069760ede5018b959c1b9dce
gowtham7672 <gowthamreddykopperla15@gmail.com>
2026-02-11 21:45:51 +0530

    Day3-Smart Counter

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,35 @@
-import ProductCard from "./components/ProductCard";
-function App(){
+import {useState} from "react";
 
-  const products = [
-    {
-      id : 1 ,
-      name : "iphone",
-      price : 80000,
-      description : "Latest Apple smart phone"
-    },
-    {
-      id : 2 ,
-      name : "samsung",
-      price : 70000,
-      description : "Latest Samsung smart phone"
-    },
-    {
-      id : 3 ,
-      name : "realme",
-      price : 50000,
-      description : "Latest realme smart phone"
+function App(){
+  const [count,setCount] = useState(0);
+
+  function increase(){
+    setCount(count+1);
+  }
+
+  function decrease(){
+    if(count<=0){
+      setCount(count-1);
     }
-  ];
+    setCount(count-1);
+  }
+
+  function reset(){
+    setCount(0);
+  }
   return (
-    <>
-      <h1 className="title">Product store</h1>
-      {products.map((product) => (<ProductCard key ={product.id} name={product.name} price={product.price} description = {product.description} />))}
-    </>
-    
+    <div className="container">
+      <h1 className={count>10?"high":"normal"}>{count}</h1>
+      <div className="buttons">
+        <button onClick={increase}>Increase</button>
+        <button onClick={decrease}>Decrease</button>
+        <button onClick={reset}>Reset</button>
+      </div>
+      {count===0 && <p>Start counting</p>}
+      {count>0 && count<=10 && <p>Keep going</p>}
+      {count<0 && <p>Try to improve your score</p>}
+      {count>10 && <p>High score</p>}
+    </div>
   );
 }
 
